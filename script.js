98b9901eef0f63463b7739995089714bc883fc4f
Oliver <oliver20669@gmail.com>
2022-03-23 15:17:31 +0100

    Commiting the operate function along with the dot feature and the equals sign

--- script.js
+++ script.js
@@ -39,3 +39,85 @@ function divide(a,b){
     }
     
 }
+
+function operate(n1,op,n2){
+
+    if(op == "+"){
+
+        n1 = add(n1,n2);
+
+    }else if (op == "-") {
+
+        n1 = subtract(n1,n2);
+
+    }else if (op == "x") {
+
+        n1 = multiply(n1,n2);
+
+    }else if (op == ":") {
+
+        n1 = (divide(n1,n2)).toFixed(2);
+
+    }
+
+    return n1;
+}
+
+function clearAll(){
+
+    operation = [];
+    display.textContent = "";
+
+}
+
+clear.addEventListener("click", clearAll);
+
+function insertDot(){
+
+    if(insDot){
+
+        display.textContent += ".";
+        insDot = false;  
+
+    }
+
+}
+
+dot.addEventListener("click", insertDot);
+
+//number typing on display
+for (let i = 0; i < nmb.length; i++) {
+
+    nmb[i].addEventListener("click", function(){
+
+        if(toClear == false){
+
+            display.textContent = display.textContent + nmb[i].textContent;
+
+        }else{
+
+            display.textContent = "";
+            display.textContent = display.textContent + nmb[i].textContent;
+            toClear = false;
+
+        }
+        
+    });
+}
+
+function equals(){
+
+    if(operation[0] !== undefined){
+        
+        operation[2] = display.textContent;
+        display.textContent = operate(operation[0],operation[1],operation[2]);
+        operation[0] = undefined;
+        operation[1] = undefined;
+        operation[2] = undefined;
+        toClear = true;
+        insDot = true;
+
+    }
+}
+
+eql.addEventListener("click", equals);
